8a1af9c12e435228171a0f61da35019a0a464396
merrickliu888 <merrickliu168@gmail.com>
2023-10-29 15:20:30 -0400

    adding setters for NutrientRange class

--- src/entity/NutrientRange.java
+++ src/entity/NutrientRange.java
@@ -13,19 +13,36 @@ public class NutrientRange {
         this.carbRange = carbRange;
     }
 
+
     public Range getCalorieRange() {
         return calorieRange;
+    }
+
+    public void setCalorieRange(Range calorieRange) {
+        this.calorieRange = calorieRange;
     }
 
     public Range getFatRange() {
         return fatRange;
     }
 
+    public void setFatRange(Range fatRange) {
+        this.fatRange = fatRange;
+    }
+
     public Range getProteinRange() {
         return proteinRange;
     }
 
+    public void setProteinRange(Range proteinRange) {
+        this.proteinRange = proteinRange;
+    }
+
     public Range getCarbRange() {
         return carbRange;
+    }
+
+    public void setCarbRange(Range carbRange) {
+        this.carbRange = carbRange;
     }
 }
