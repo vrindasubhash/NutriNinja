296cade333989e202f71e47475290d8fd6aba305
merrickliu888 <merrickliu168@gmail.com>
2023-10-29 14:55:28 -0400

    ingredient entity

---
+++ src/entity/Ingredient.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class Ingredient {
+    private String name;
+    private int amount;
+    private String unit;
+
+    public Ingredient(String name, int amount, String unit) {
+        this.name = name;
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+}
