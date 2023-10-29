c114e237e9db7da2fadf3a151e2b5e029367f6ef
merrickliu888 <merrickliu168@gmail.com>
2023-10-29 15:20:45 -0400

    adding setters for Range class

--- src/entity/Range.java
+++ src/entity/Range.java
@@ -9,12 +9,19 @@ public class Range {
         this.upperBound = upperBound;
     }
 
-
     public int getLowerBound() {
         return lowerBound;
     }
 
+    public void setLowerBound(int lowerBound) {
+        this.lowerBound = lowerBound;
+    }
+
     public int getUpperBound() {
         return upperBound;
+    }
+
+    public void setUpperBound(int upperBound) {
+        this.upperBound = upperBound;
     }
 }
