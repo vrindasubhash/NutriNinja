90937af4d56a55bb382a8a48266d316e51c6f424
merrickliu888 <merrickliu168@gmail.com>
2023-10-29 15:18:31 -0400

    adding setters for ingredient class

--- src/entity/Ingredient.java
+++ src/entity/Ingredient.java
@@ -15,11 +15,23 @@ public class Ingredient {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getAmount() {
         return amount;
     }
 
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
     public String getUnit() {
         return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
 }
