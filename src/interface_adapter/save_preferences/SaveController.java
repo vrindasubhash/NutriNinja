478e18034fc67003d32deb95c238380d5a324ff2
samaslanowicz <sam.aslan01@yahoo.ca>
2023-11-05 12:59:45 -0800

    initialized the SaveController and added a first draft of the execute() method

--- src/interface_adapter/save_preferences/SaveController.java
+++ src/interface_adapter/save_preferences/SaveController.java
@@ -1,3 +1,18 @@
 package interface_adapter.save_preferences;
 
-public class SaveController{}
+import entity.NutrientRange;
+import use_case.save_preferences.SaveInputBoundary;
+import use_case.save_preferences.SaveInputData;
+
+public class SaveController{
+    final SaveInputBoundary saveUseCaseInteractor;
+    public SaveController(SaveInputBoundary saveUseCaseInteractor) {
+        this.saveUseCaseInteractor = saveUseCaseInteractor;
+    }
+    public void execute(NutrientRange nutrientRange, String healthPreferences,
+                        String dietPreference,
+                        String cuisinePreference) {
+        SaveInputData saveInputData = new SaveInputData(nutrientRange, healthPreferences, dietPreference, cuisinePreference);
+        saveUseCaseInteractor.execute(saveInputData);
+    }
+}
