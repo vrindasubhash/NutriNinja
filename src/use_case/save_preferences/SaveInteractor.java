a049fd15042be0a64441f0f56c48852ce75aebe9
samaslanowicz <sam.aslan01@yahoo.ca>
2023-11-05 12:55:51 -0800

    Initialized the SaveInteractor and added a first draft of an execute() method, that calls a method savePreferences(), which has not yet been created

--- src/use_case/save_preferences/SaveInteractor.java
+++ src/use_case/save_preferences/SaveInteractor.java
@@ -1,4 +1,20 @@
 package use_case.save_preferences;
+    public class SaveInteractor implements SaveInputBoundary {
+        final SaveDataAccessInterface saveDataAccessObject;
+        final SaveOutputBoundary savePresenter;
 
-public class SaveInteractor {
+
+    public SaveInteractor(SaveDataAccessInterface saveDataAccessObject,
+                          SaveOutputBoundary savePresenter){
+        this.saveDataAccessObject = saveDataAccessObject;
+        this.savePresenter = savePresenter;
+    }
+
+    @Override
+    public void execute(SaveInputData saveInputData) {
+        saveDataAccessObject.savePreferences();
+        SaveOutputData saveOutputData = new SaveOutputData();
+        savePresenter.prepareSuccessView(saveOutputData);
+    }
+
 }
