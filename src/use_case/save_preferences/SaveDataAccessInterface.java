1dd01966076017e4b9b33939e02a1bcda0b29308
samaslanowicz <sam.aslan01@yahoo.ca>
2023-11-05 13:00:43 -0800

    Refactored the SaveDataAccessInterface interface

---
+++ src/use_case/save_preferences/SaveDataAccessInterface.java
@@ -0,0 +1,4 @@
+package use_case.save_preferences;
+
+public interface SaveDataAccessInterface {
+}
--- src/use_case/save_preferences/SaveUserDataAccessInterface.java
+++
@@ -1,4 +0,0 @@
-package use_case.save_preferences;
-
-public interface SaveUserDataAccessInterface {
-}
