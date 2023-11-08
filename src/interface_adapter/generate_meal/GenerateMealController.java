db667a0ac6981279bcbc1d7f36a2bee82d298cf5
merrickliu888 <merrickliu168@gmail.com>
2023-11-08 10:56:25 -0500

    Constructing Range in execute()

--- src/interface_adapter/generate_meal/GenerateMealController.java
+++ src/interface_adapter/generate_meal/GenerateMealController.java
@@ -1,6 +1,7 @@
 package interface_adapter.generate_meal;
 
 import java.util.List;
+import app.custom_data.Range;
 
 public class GenerateMealController {
     final GenerateMealInputBoundary generateMealUseCaseInteractor;
@@ -9,15 +10,19 @@ public class GenerateMealController {
         this.generateMealUseCaseInteractor = generateMealUseCaseInteractor;
     }
 
-    public void execute(List<String> health, List<String> mealType, List<String> dishType, int calMin, int calMax, int carbMin, int carbMax, int proteinMin, int proteinMax, int fatMin, int fatMax) {
+    public void execute(List<String> healthPreferences, List<String> mealType, List<String> dishType, int calMin, int calMax, int carbMin, int carbMax, int proteinMin, int proteinMax, int fatMin, int fatMax) {
+        Range calRange = new Range(calMin, calMax);
+        Range carbRange = new Range(carbMin, carbMax);
+        Range proteinRange = new Range(proteinMin, proteinMax);
+        Range fatRange = new Range(fatMin, fatMax);
         GenerateMealInputData generateMealInputData = new GenerateMealInputData(
-                health,
+                healthPreferences,
                 mealType,
                 dishType,
-                calMin, calMax,
-                carbMin, carbMax,
-                proteinMin, proteinMax,
-                fatMin, fatMax
+                calRange,
+                carbRange,
+                proteinRange,
+                fatRange
         );
         generateMealUseCaseInteractor.execute(generateMealInputData);
     }
