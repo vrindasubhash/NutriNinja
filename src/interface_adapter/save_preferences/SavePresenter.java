cdaea177a09a67963d8241d73dbdfdeb2ffbdb7c
samaslanowicz <sam.aslan01@yahoo.ca>
2023-11-10 20:46:30 -0800

    First draft of the SavePresenter

--- src/interface_adapter/save_preferences/SavePresenter.java
+++ src/interface_adapter/save_preferences/SavePresenter.java
@@ -1,4 +1,19 @@
 package interface_adapter.save_preferences;
 
-public class SavePresenter {
+import use_case.save_preferences.SaveOutputBoundary;
+import use_case.save_preferences.SaveOutputData;
+
+public class SavePresenter implements SaveOutputBoundary {
+    private final SaveViewModel saveViewModel;
+
+    public SavePresenter(SaveViewModel saveViewModel){
+        this.saveViewModel = saveViewModel;
+    }
+    @Override
+    public void prepareSuccessView(SaveOutputData response) {
+        // On success, switch to the logged in view.
+
+        SaveState saveState = saveViewModel.getState();
+        this.saveViewModel.setState(saveState);
+    }
 }
