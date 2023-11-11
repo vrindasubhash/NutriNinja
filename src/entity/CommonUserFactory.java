075bf635a108e2fb0603713ddab6813716ada7c9
merrickliu888 <merrickliu168@gmail.com>
2023-11-11 15:24:19 -0500

    UserFactory interface and CommonUserFactory

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -1,4 +1,19 @@
 package entity;
 
+import app.custom_data.Range;
+
+import java.util.List;
+
 public class CommonUserFactory implements UserFactory{
+    @Override
+    public User create(String username, String password, List<String> healthPreference, List<String> dishType, int[] calRange, int[] fatRange, int[] proteinRange, int[] carbRange) {
+        NutrientRange nutrientRange = new NutrientRange(
+                new Range(calRange[0], calRange[1]),
+                new Range(fatRange[0], fatRange[1]),
+                new Range(proteinRange[0], proteinRange[1]),
+                new Range(carbRange[0], carbRange[1])
+        );
+        UserPreference userPreference = new UserPreference(nutrientRange, healthPreference, dishType);
+        return new CommonUser(username, password, userPreference);
+    }
 }
--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,4 +1,15 @@
 package entity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface UserFactory {
+    User create(String username,
+                String password,
+                List<String> healthPreference,
+                List<String> dishType,
+                int[] calRange,
+                int[] fatRange,
+                int[] proteinRange,
+                int[] carbRange);
 }
