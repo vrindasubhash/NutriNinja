fcbe13032882fc9872c25f0626086854cf1e2e17
samaslanowicz <sam.aslan01@yahoo.ca>
2023-11-10 20:46:48 -0800

    First draft of the SaveViewModel

--- src/interface_adapter/save_preferences/SaveViewModel.java
+++ src/interface_adapter/save_preferences/SaveViewModel.java
@@ -1,4 +1,20 @@
 package interface_adapter.save_preferences;
 
-public class SaveViewModel {
+public class SaveViewModel extends ViewModel{
+
+    public final String TITLE_LABEL = "Preferences";
+    public final String NUTRIENT_LABEL = "Enter Preferred Nutrient Values";
+    public final String HEALTH_PREF_LABEL = "Enter Health Preferences";
+    public final String DISH_TYPE_LABEL = "Enter Breakfast, Lunch, or Dinner";
+
+    public static final String SAVE_PREFERENCES_BUTTON_LABEL = "Save Preferences";
+    public static final String GENERATE_MEAL_BUTTON_LABEL = "Generate Meal";
+
+    private SaveState state = new SaveState();
+
+    private SaveState getState(){
+        return state;
+    }
+
+
 }
