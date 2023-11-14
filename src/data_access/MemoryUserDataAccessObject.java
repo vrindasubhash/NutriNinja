10cbb55702eedfb697140c44b5a950f4bc71d5f0
merrickliu888 <merrickliu168@gmail.com>
2023-11-14 00:15:36 -0500

    created in memory dao

---
+++ src/data_access/MemoryUserDataAccessObject.java
@@ -0,0 +1,46 @@
+package data_access;
+
+import entity.NutrientRange;
+import entity.User;
+import entity.UserPreference;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDataAccessObject implements LoginUserDataAccessInterface,
+                                                   SignupDataAccessInterface,
+                                                   SavePreferencesDataAccessInterface {
+    private final Map<String, User> accounts = new HashMap<>();
+
+    /**
+     * Saves user to database
+     * @param user represents the user to save
+     */
+    @Override
+    public void saveUser(User user) {
+        accounts.put(user.getUsername(), user);
+    }
+
+    /**
+     * Returns true if the user exists and false otherwise
+     * @param username identifies the user you want to check exists
+     * @return a boolean representing if the user exists or not
+     */
+    @Override
+    public boolean userExists(String username) {
+        return accounts.containsKey(username);
+    }
+
+    /**
+     * Returns a user object representing the user
+     * @param username identifies the user
+     * @return the user
+     */
+    @Override
+    public User getUser(String username) {
+        return accounts.get(username);
+    }
+}
