346a6eed504f4bb4cda72be5fc1dee90ffc99962
Vrinda Subhash <vrindasubhashc@gmail.com>
2023-11-15 23:23:12 -0500

    Renamed existsByName to userExists based on code review

--- src/use_case/login/LoginUserDataAccessInterface.java
+++ src/use_case/login/LoginUserDataAccessInterface.java
@@ -3,7 +3,7 @@ package use_case.login;
 import entity.User;
 
 public interface LoginUserDataAccessInterface {
-    boolean existsByName(String identifier);
+    boolean userExists(String identifier);
 
     User get(String username);
 }
