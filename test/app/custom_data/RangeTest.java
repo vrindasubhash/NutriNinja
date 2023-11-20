34241ff918ac6595ced1750316e94729ab2f7b81
Vrinda Subhash <vrindasubhashc@gmail.com>
2023-11-20 13:58:57 -0500

    Updated the login usecase test cases to test the userpreferences values too based on code review. Used the CommonUserFactory constructor instead of CommonUser. Also removed an uneeded import from RangeTest.java

--- test/app/custom_data/RangeTest.java
+++ test/app/custom_data/RangeTest.java
@@ -2,7 +2,6 @@ package app.custom_data;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.*;
 
--- test/use_case/login/LoginInteractorTest.java
+++ test/use_case/login/LoginInteractorTest.java
@@ -1,10 +1,18 @@
 package use_case.login;
 
+import app.custom_data.Range;
 import data_access.MemoryUserDataAccessObject;
 import entity.CommonUser;
+import entity.CommonUserFactory;
+import entity.NutrientRange;
+import entity.UserPreferences;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.prefs.Preferences;
 
 import static org.junit.Assert.*;
 
@@ -16,22 +24,34 @@ public class LoginInteractorTest {
     @Before
     public void setUp() throws Exception {
         this.userDataAccessObject = new MemoryUserDataAccessObject();
-        this.userDataAccessObject.saveUser(new CommonUser(this.firstUser, this.firstPassword, null));
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        this.userDataAccessObject = null;
+        this.userDataAccessObject.saveUser(new CommonUserFactory().create(this.firstUser, this.firstPassword));
     }
 
     @Test
     public void testUserExistsAndPasswordCorrect() {
         String name = this.firstUser;
         String password = this.firstPassword;
+
         LoginOutputBoundary loginPresenter = new LoginOutputBoundary() {
             @Override
             public void prepareSuccessView(LoginOutputData user) {
                 assertEquals(name, user.getUsername());
+
+                // Testing preferences
+                UserPreferences userPreferences = user.getUserPreferences();
+                List<String> emptyList = new ArrayList<>();
+                assertEquals(emptyList, userPreferences.getHealthPreferences());
+                assertEquals(emptyList, userPreferences.getDishType());
+
+                NutrientRange nutrientRange = user.getUserPreferences().getNutrientRange();
+                Range<Integer> calRange = nutrientRange.getCalorieRange();
+                Range<Integer> fatRange = nutrientRange.getFatRange();
+                Range<Integer> proteinRange = nutrientRange.getProteinRange();
+                Range<Integer> carbRange = nutrientRange.getCarbRange();
+                assertArrayEquals(new int[]{0, 2000}, new int[]{calRange.getLowerBound(), calRange.getUpperBound()});
+                assertArrayEquals(new int[]{0, 100}, new int[]{fatRange.getLowerBound(), fatRange.getUpperBound()});
+                assertArrayEquals(new int[]{0, 150}, new int[]{proteinRange.getLowerBound(), proteinRange.getUpperBound()});
+                assertArrayEquals(new int[]{0, 500}, new int[]{carbRange.getLowerBound(), carbRange.getUpperBound()});
             }
 
             @Override
