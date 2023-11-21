e381990bb95d9ce237654ffc3841196d4746f106
Jack <junwei.fan@mail.utoronto.ca>
2023-11-21 14:39:31 -0500

    Finished GenerateRandomMealController

--- src/interface_adapter/generate_random_meal/GenerateRandomMealController.java
+++ src/interface_adapter/generate_random_meal/GenerateRandomMealController.java
@@ -1,6 +1,21 @@
 package interface_adapter.generate_random_meal;
 
+import use_case.generate_meal.GenerateMealInputBoundary;
+import use_case.generate_meal_by_id.GenerateMealByIDInputBoundary;
+import use_case.generate_meal_by_id.GenerateMealByIDInputData;
+import use_case.generate_meal_by_id.GenerateMealByIDInteractor;
+
 public class GenerateRandomMealController {
 
+    final GenerateMealByIDInteractor generateMealByIDInteractor;
+
+    public GenerateRandomMealController(GenerateMealByIDInputBoundary generateMealUseCaseInteractor) {
+        this.generateMealByIDInteractor = generateMealUseCaseInteractor;
+    }
+
+    public void execute(String mealID){
+        GenerateMealByIDInputData inputData = new GenerateMealByIDInputData(mealID);
+        generateMealByIDInteractor.execute(inputData);
+    }
 
 }
