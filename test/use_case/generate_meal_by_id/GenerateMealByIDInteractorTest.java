e708c140469b2fc5009610d2a207358f0c1bf98d
merrickliu888 <merrickliu168@gmail.com>
2023-11-22 00:29:46 -0500

    Test file

---
+++ test/use_case/generate_meal_by_id/GenerateMealByIDInteractorTest.java
@@ -0,0 +1,17 @@
+package use_case.generate_meal_by_id;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GenerateMealByIDInteractorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
