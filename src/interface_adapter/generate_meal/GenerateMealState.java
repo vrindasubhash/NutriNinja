d768cd9dabb8b76b7fae53dd739f947929e50998
Jack <junwei.fan@mail.utoronto.ca>
2023-11-26 11:19:20 -0500

    GenerateMealState Finished

--- src/interface_adapter/generate_meal/GenerateMealState.java
+++ src/interface_adapter/generate_meal/GenerateMealState.java
@@ -2,94 +2,117 @@ package interface_adapter.generate_meal;
 
 public class GenerateMealState {
 
-    public String getMeal_label() {
-        return meal_name;
+    public String getMealName() {
+        return mealName;
     }
 
-    public void setMeal_label(String meal_label) {
-        this.meal_name = meal_label;
+    public void setMealName(String meal_label) {
+        this.mealName = meal_label;
     }
 
-    public String getImage_url() {
-        return image_url;
+    public String getImageURL() {
+        return imageUrl;
     }
 
-    public void setImage_url(String image_url) {
-        this.image_url = image_url;
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 
-    public double getMeal_calories() {
-        return meal_calories;
+    public double getMealCalories() {
+        return mealCalories;
     }
 
-    public void setMeal_calories(double meal_calories) {
-        this.meal_calories = meal_calories;
+    public void setMealCalories(double mealCalories) {
+        this.mealCalories = mealCalories;
     }
 
-    public double getMeal_protein() {
-        return meal_protein;
+    public double getMealProtein() {
+        return mealProtein;
     }
 
-    public void setMeal_protein(double meal_protein) {
-        this.meal_protein = meal_protein;
+    public void setMealProtein(double mealProtein) {
+        this.mealProtein = mealProtein;
     }
 
-    public double getMeal_carbs() {
-        return meal_carbs;
+    public double getMealCarbs() {
+        return mealCarbs;
     }
 
-    public void setMeal_carbs(double meal_carbs) {
-        this.meal_carbs = meal_carbs;
+    public void setMealCarbs(double mealCarbs) {
+        this.mealCarbs = mealCarbs;
     }
 
-    public double getMeal_fat() {
-        return meal_fat;
+    public double getMealFat() {
+        return mealFat;
     }
 
-    public void setMeal_fat(double meal_fat) {
-        this.meal_fat = meal_fat;
+    public void setMealFat(double mealFat) {
+        this.mealFat = mealFat;
     }
 
-    public String getIngredients_label() {
-        return ingredients_label;
+    public String getIngredientsLabel() {
+        return ingredientsLabel;
     }
 
-    public void setIngredients_label(String ingredients_label) {
-        this.ingredients_label = ingredients_label;
+    public void setIngredientsLabel(String ingredientsLabel) {
+        this.ingredientsLabel = ingredientsLabel;
     }
 
-    public String getRecipe_label() {
-        return recipe_label;
+    public String getRecipeURL() {
+        return recipeUrl;
     }
 
-    public void setRecipe_label(String recipe_label) {
-        this.recipe_label = recipe_label;
+    public void setRecipeUrl(String recipeUrl) {
+        this.recipeUrl = recipeUrl;
     }
 
-    public String meal_name = "";
-    public String image_url = ""; //URL of image
-    public double meal_calories = 0;
-    public double meal_protein = 0;
-    public double meal_carbs = 0;
-    public double meal_fat = 0;
+    public String getRecipeSource() {
+        return recipeSource;
+    }
+
+    public void setRecipeSource(String recipe_source) {
+        this.recipeSource = recipe_source;
+    }
+
+    public String getAPIError() {
+        return APIError;
+    }
+
+    public void setAPIError(String APIError) {
+        this.APIError = APIError;
+    }
+
+
+    public String mealName = "";
+    public String imageUrl = ""; //URL of image
+    public double mealCalories = 0;
+    public double mealProtein = 0;
+    public double mealCarbs = 0;
+    public double mealFat = 0;
 
 
     //INGREDIENTS
-    public String ingredients_label = "";
+    public String ingredientsLabel = "";
 
 
     //Recipe
-    public String recipe_label = ""; //url of recipe
+    public String recipeUrl = ""; //url of recipe
+    public String recipeSource = ""; //source of recipe
+
+    //Error
+    public String APIError = "";
 
     public GenerateMealState(GenerateMealState copy){
-        this.meal_name = copy.meal_name;
-        this.image_url = copy.image_url;
-        this.meal_calories = copy.meal_calories;
-        this.meal_protein = copy.meal_protein;
-        this.meal_carbs = copy.meal_carbs;
-        this.meal_fat = copy.meal_fat;
-        this.ingredients_label = copy.ingredients_label;
-        this.recipe_label = copy.recipe_label;
+        this.mealName = copy.mealName;
+        this.imageUrl = copy.imageUrl;
+        this.mealCalories = copy.mealCalories;
+        this.mealProtein = copy.mealProtein;
+        this.mealCarbs = copy.mealCarbs;
+        this.mealFat = copy.mealFat;
+        this.ingredientsLabel = copy.ingredientsLabel;
+        this.recipeUrl = copy.recipeUrl;
+        this.recipeSource =copy.recipeSource;
+        this.APIError = copy.APIError;
     }
 
     //Explicit default constructor
