e1f5cbc853d1dc504c934970ac7ba77f4b1c1f98
merrickliu888 <merrickliu168@gmail.com>
2023-11-27 19:57:24 -0500

    test file

---
+++ test/data_access/FileUserDataAccessObjectTest.java
@@ -0,0 +1,38 @@
+package data_access;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileUserDataAccessObjectTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void saveUser() {
+    }
+
+    @Test
+    public void saveUserPreferences() {
+    }
+
+    @Test
+    public void userExists() {
+    }
+
+    @Test
+    public void getUser() {
+    }
+}
