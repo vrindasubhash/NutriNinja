d7d651fc6ee408fbb1871a25f40b8e038aa68a79
merrickliu888 <merrickliu168@gmail.com>
2023-11-27 20:04:05 -0500

    tear down and set up

--- test/data_access/FileUserDataAccessObjectTest.java
+++ test/data_access/FileUserDataAccessObjectTest.java
@@ -1,8 +1,13 @@
 package data_access;
 
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
 
 import static org.junit.Assert.*;
 
@@ -10,14 +15,26 @@ public class FileUserDataAccessObjectTest {
 
     @Before
     public void setUp() throws Exception {
+        UserFactory userFactory = new CommonUserFactory();
+        FileUserDataAccessObject dao = new FileUserDataAccessObject("./test_data", userFactory);
+
+        User testUser = userFactory.create("test_username", "test_password");
+        dao.saveUser(testUser);
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
+        File testData = new File("./test_data");
+        if (testData.delete()) {
+            System.out.println("Deleted test_data.csv successfully.");
+        } else {
+            System.out.println("Deleted test_data.csv unsuccessfully.");
+        }
     }
 
     @Test
     public void save() {
+
     }
 
     @Test
