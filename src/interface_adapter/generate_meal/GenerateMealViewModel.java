dc2d0eb13d3cd079701b8eed1b59b9b4ebbf85cf
Jack <junwei.fan@mail.utoronto.ca>
2023-11-30 19:14:03 -0500

    Minor refactors

--- src/interface_adapter/generate_meal/GenerateMealViewModel.java
+++ src/interface_adapter/generate_meal/GenerateMealViewModel.java
@@ -11,33 +11,25 @@ public class GenerateMealViewModel extends ViewModel {
 
 
     //HEADERS
-    public static final String MEAL_HEADER = "Meal: ";
+    public static final String MEAL_HEADER = "Meal Suggested: ";
     public static final String SERVINGS_HEADER = "Servings: ";
     public static final String CALORIES_HEADER = "Calories: ";
     public static final String PROTEIN_HEADER = "Protein: ";
     public static final String CARBS_HEADER = "Carbs: ";
     public static final String FAT_HEADER = "Fat: ";
-    public final static String INGREDIENTS_HEADER = "Ingredients";
-    public final static String RECIPE_LINK_HEADER = "Recipe Link";
-    public final static String RECIPE_SOURCE_HEADER = "Recipe Source";
+    public final static String INGREDIENTS_HEADER = "Ingredients: ";
+    public final static String RECIPE_LINK_HEADER = "Recipe Link: ";
+    public final static String RECIPE_SOURCE_HEADER = "Recipe Source:";
 
 
 
-    //LABELS
-    public static final String MEAL_LABEL = "Meal: ";
-    public static final String SERVINGS_LABEL = "Servings: ";
-    public static final String CALORIES_LABEL = "";
-    public static final String PROTEIN_LABEL = "";
-    public static final String CARBS_LABEL = "";
-    public static final String FAT_LABEL = "";
-    public static final String INGREDIENTS_LABEL = "";
-    public static final String RECIPE_LINK_LABEL = "";
-    public static final String RECIPE_SOURCE_LABEL = "";
+
 
 
     //Buttons
-    public String BACK_BUTTON_LABEL = "Back";
-    public String REGENERATE_BUTTON_LABEL = "Generate Another Meal";
+    public final static String BACK_BUTTON_LABEL = "Back";
+    public final static String REGENERATE_BUTTON_LABEL = "Generate Another Meal";
+    public final static String FEELING_LUCKY_BUTTON_LABEL = "I'm Feeling Lucky";
 
     private GenerateMealState state= new GenerateMealState();
 
@@ -51,7 +43,7 @@ public class GenerateMealViewModel extends ViewModel {
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public GenerateMealViewModel(String viewName) {
-        super("Generate Meal View");
+        super("Generate Meal");
     }
 
     @Override
